02f390332c8527268a3511bfbcec1e5ee8a923dc
FathyhQP <107481365+FathyhQP@users.noreply.github.com>
2025-02-19 21:42:57 +0700

    index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ const appMiddleware = require("./middleware/");
 const app = express();
 const port = process.env.PORT != null ? parseInt(process.env.PORT) : 3001;
 
-app.listen(port,() => {
+app.listen(port, 3000, '0.0.0.0', () => {
   console.log(`😁 Welcome to Server!`);
+  console.log(`Server running on http://0.0.0.0:3000`);
   console.log(`🚀 Listening on port ${port}`);
 });
 
