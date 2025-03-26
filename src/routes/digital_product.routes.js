3a9de2bc1404ca6343ce43fe57fc9d49d32663d3
zeinirfansyah <12.zeinirfansyah@gmail.com>
2025-03-26 18:14:43 +0700

    feat: implement delete functionality for digital products and update routing structure

--- src/routes/digital_product.routes.js
+++ src/routes/digital_product.routes.js
@@ -3,13 +3,15 @@ const { verifyToken, authorizeRole } = require("../middleware/auth");
 const { getAllProducts } = require("../controllers/digital_product_controllers/get_all_product.controller");
 const { updateProduct } = require("../controllers/digital_product_controllers/update_product.controller");
 const { createProduct } = require("../controllers/digital_product_controllers/create_product.controller");
+const deleteProduct = require("../controllers/digital_product_controllers/delete_product.controller");
 
 
-const meRouter = Router();
+const productRouter = Router();
 
-meRouter.get("/digital-product", getAllProducts);
-meRouter.patch("/digital-product/:id", verifyToken, authorizeRole("ADMIN"), updateProduct);
-meRouter.post("/digital-product", verifyToken, authorizeRole("ADMIN"), createProduct);
+productRouter.get("/digital-product", getAllProducts);
+productRouter.patch("/digital-product/:id", verifyToken, authorizeRole("ADMIN"), updateProduct);
+productRouter.post("/digital-product", verifyToken, authorizeRole("ADMIN"), createProduct);
+productRouter.delete("/digital-product/:id", verifyToken, authorizeRole("ADMIN"), deleteProduct);
 
 
-module.exports = meRouter;
+module.exports = productRouter;
