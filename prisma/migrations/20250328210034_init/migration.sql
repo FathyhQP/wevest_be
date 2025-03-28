3c7c14e861cc04e1c0b19faf0e6038b8f5233979
zeinirfansyah <12.zeinirfansyah@gmail.com>
2025-03-29 06:17:15 +0700

    feat: restructure database migrations and seed files for User and DigitalProduct models

--- prisma/migrations/20250328210034_init/migration.sql
+++ prisma/migrations/20250328210034_init/migration.sql
@@ -1,4 +1,27 @@
 -- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `user_code` VARCHAR(191) NOT NULL,
+    `fullname` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `phone` VARCHAR(191) NOT NULL,
+    `job_status` VARCHAR(191) NOT NULL,
+    `field` VARCHAR(191) NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `avatar` VARCHAR(191) NULL,
+    `role` ENUM('CUSTOMER', 'ADMIN') NOT NULL DEFAULT 'CUSTOMER',
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_user_code_key`(`user_code`),
+    UNIQUE INDEX `User_username_key`(`username`),
+    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_phone_key`(`phone`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `DigitalProduct` (
     `id` VARCHAR(191) NOT NULL,
     `title` VARCHAR(191) NOT NULL,
@@ -7,6 +30,7 @@ CREATE TABLE `DigitalProduct` (
     `file_path` VARCHAR(191) NOT NULL,
     `thumbnail` VARCHAR(191) NULL,
     `category` VARCHAR(191) NOT NULL,
+    `tag` VARCHAR(191) NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
 
--- prisma/seed.js
+++ prisma/seed.js
@@ -1,12 +1,17 @@
 const { PrismaClient } = require("@prisma/client");
 const { seedUser } = require("./seeds/user.seed");
+const { seedProducts } = require("./seeds/product.seed");
 
 const prisma = new PrismaClient();
 
 async function main() {
   try {
-    console.log("Seeding...");
+    console.log("Seeding users...");
     await seedUser();
+
+    console.log("Seeding products...");
+    await seedProducts();
+
     console.log("Seeding Completed");
   } catch (error) {
     console.error("Error during seeding:", error);
