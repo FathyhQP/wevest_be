15fdb2ecdac89bb0d471c522b077ce4647fcd704
zeinirfansyah <12.zeinirfansyah@gmail.com>
2025-04-11 00:44:00 +0700

    feat: implement create product transaction functionality and update route import

--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { verifyToken } = require("../middleware/auth");
-const { createProductTransaction } = require("../controllers/transaction_controllers/create_product_transaction.controller.");
+const { createProductTransaction } = require("../controllers/transaction_controllers/create_product_transaction.controller");
 
 const transactionRouter = Router();
 
